01da66faf951b17d63a789e6bb3792709e8d093c
Thammaruthai <thammaruthai@gmail.com>
2024-11-06 12:08:36 +0700

    feat: add answer ex4

--- ex-4.js
+++ ex-4.js
@@ -1,20 +1,26 @@
 function findOdd(nums) {
   // Start coding here
-  let oddCount=0
-  let oddIndex =[]
-  for (let index = 0; index < nums.length; index++) {
-    if(nums[index]!=nums[index+1] && nums[index]%2===1){
-      oddCount++;
-    }
-  }
-    
-  return oddCount
-  
+   let numCount = {};
+   let result = null;
+   for (let i = 0; i < nums.length; i++) {
+     if (numCount[nums[i]] == undefined) {
+       numCount[nums[i]] = 1;
+     } else numCount[nums[i]]++;
+   }
+
+     for (let i in numCount) {
+       if (numCount[i] %2===1) {
+         result = Number(i);
+         return result;
+       }
+     }
+   
+   
 }
 
 let result1 = findOdd([0]);
-let result2 = findOdd([1, 1, 2]);
-let result3 = findOdd([1, 2, 2, 3, 3, 4, 3, 3, 1]); 
+let result2 = findOdd([1, 1, 2]); //ทำไมอันนี้ 2 
+let result3 = findOdd([1, 2, 2, 3, 3, 4, 3, 3, 1]);  //แต่อันนี้ 4
 
 console.log(result1); // 0
 console.log(result2); // 2
